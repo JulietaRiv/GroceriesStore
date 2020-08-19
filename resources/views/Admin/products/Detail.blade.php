481f9c5cf87ad69d3d92ab4a37df48e286c08a03
JulietaRiv <positivejvr@gmail.com>
2020-08-19 15:35:47 -0300

    avances detail product view

--- resources/views/Admin/products/Detail.blade.php
+++ resources/views/Admin/products/Detail.blade.php
@@ -11,23 +11,30 @@
     <div class="col-md-12">
         <div class="box">
             <div class="box-header with-border">
-            <h3 class="box-title">Producto {{ $product->name }}</h3>
+            <h3 class="box-title">Producto:  {{ $product->name }}</h3>
             </div>
+            <br>
             <!-- /.box-header -->
             <div class="box-body">
                 <table class="table table-bordered">
                     <tbody>
-                        <tr>
-                            <th>Nombre</th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>          
+                        <ul>
+                            <!-- /aca va la foto -->
+                            <li>Categoría: {{ $product->category->name }}</li>
+                            <li>Marca: {{ $product->brand->name }}</li>
+                            <li>Precio: $ {{ $product->price }}</li>
+                            <li>Precio promo: $ {{ $product->promo_price }}</li>
+                            <li>Descripción: {{ $product->description }}</li>
+                            <li>Stock: {{ $product->stock  == 1 ? 'Si' : 'No' }}</li>
+                            <li>Oferta: {{ $product->offer  == 1 ? 'Si' : 'No' }}</li>
+                            <li>Destacado: {{ $product->highlighted  == 1 ? 'Si' : 'No' }}</li>
+                            <li>Sin Tacc: {{ $product->celiacs  == 1 ? 'Si' : 'No' }}</li>
+                            <li>Orgánico: {{ $product->organic  == 1 ? 'Si' : 'No' }}</li>
+                            <li>Agroecológico: {{ $product->agroecological  == 1 ? 'Si' : 'No' }}</li>
+                            <li>Vegano: {{ $product->vegan  == 1 ? 'Si' : 'No' }}</li>
+                            <li>Activo: {{ $product->active  == 1 ? 'Si' : 'No' }}</li>
+                            <!-- /aca va las presentaciones -->
+                        </ul>
                     </tbody>
                 </table>
             </div>
--- resources/views/Site/aboutUs.blade.php
+++ resources/views/Site/aboutUs.blade.php
@@ -1,3 +1,4 @@
+
 <div id="aboutUs" class="brands">
 		<div class="container">
 		<h3>Nosotros</h3>
@@ -6,11 +7,11 @@
                     Comercializamos los mejores productos agroecológicos, orgánicos y naturales, acercando el productor al consumidor.</p><br>
                 <p>Hacenos tu pedido!</p><br>
                 <p>Envíos sin Costo: Beccar, San Isidro, Acassuso, Martínez, La Lucila, Olivos. 
-                Entrega Miércoles y Sábados de 12 a 20 hs. Compra mínima $500.</p>
+                Entrega: Miércoles y Sábados de 12 a 20 hs. Compra mínima $500.</p>
                 <p>Otras zonas, consultar costo de envío.</p><br>
                 <p>Retiro por Sucursal (Martínez): De Lunes a Sábado, sin monto de compra mínima, horario a convenir.</p><br>
                 <p>Medios de Pago: Efectivo contra entrega, Transferencia bancaria, Mercado Pago.</p>
 				<div class="clearfix"></div>
 			</div>
 		</div>
-	</div>
+</div>
