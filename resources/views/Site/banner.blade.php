9b21fb1c5f70746b8454db3fa0c68c942044654a
JulietaRiv <positivejvr@gmail.com>
2020-08-22 18:52:34 -0300

    imagenes muestra banner

--- resources/views/Site/banner.blade.php
+++ resources/views/Site/banner.blade.php
@@ -3,17 +3,17 @@
 	<div class="container">
 		<div class="col-md-6 ban-bottom3">
 			<div class="ban-top">
-				<img src="images/p2.jpg" class="img-responsive" alt="">	
+				<img src="site/images/essential-oil.jpg" class="img-responsive" alt="">	
 			</div>
 			<div class="ban-img">
 				<div class=" ban-bottom1">
 					<div class="ban-top">
-						<img src="images/p3.jpg" class="img-responsive" alt="">							
+						<img src="site/images/juice.jpg" class="img-responsive" alt="">							
 					</div>
 				</div>
 				<div class="ban-bottom2">
 					<div class="ban-top">
-						<img src="images/p4.jpg" class="img-responsive" alt="">										
+						<img src="site/images/polen.jpg" class="img-responsive" alt="">										
 					</div>
 				</div>
 				<div class="clearfix"></div>
@@ -21,7 +21,7 @@
 		</div>
 		<div class="col-md-6 ban-bottom">
 			<div class="ban-top">
-				<img src="images/111.jpg" class="img-responsive" alt="">														
+				<img src="site/images/honey.jpg" class="img-responsive" alt="">														
 			</div>
 		</div>
 						
