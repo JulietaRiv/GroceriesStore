5bab80e7a5962d6cee2c9ee15a9eb8c8afd3a2da
JulietaRiv <positivejvr@gmail.com>
2020-08-26 12:25:20 -0300

    mas dummies

--- resources/views/Site/categoriesSite.blade.php
+++ resources/views/Site/categoriesSite.blade.php
@@ -21,26 +21,26 @@
 				</div>																																												
 			</div>
 			<div class="col-md-8 products-right">
-				<div class="products-right-grid">
-					<div class="products-right-grids">
-						<div class="sorting">
-							<select id="country" onchange="change_country(this.value)" class="frm-field required sect">
-								<option selected value="precio_desc">Ordenar por menor precio</option>
-								<option value="precio_asc">Ordenar por mayor precio</option>
-								<option value="alf_a">Ordenar por A-Z</option>
-							</select>
-						</div>
-						<div class="sorting-left">
-							<select id="country1" onchange="change_country(this.value)" class="frm-field required sect">
-								<option value="">Items por página 9</option>
-								<option value="">Items por página 18</option> 
-								<option value="">Items por página 32</option>					
-								<option value="">Todo</option>								
-							</select>
-						</div>
-						<div class="clearfix"> </div>
+   			  <div class="products-right-grid">
+				<div class="products-right-grids">
+					<div class="sorting">
+						<select id="order" name="order" onchange="javascript:handleSelect(this)" class="frm-field required sect">
+							<option selected value="precio_desc">Ordenar por menor precio</option>
+							<option value="precio_asc">Ordenar por mayor precio</option>
+							<option value="alf_a">Ordenar por A-Z</option>
+						</select>
 					</div>
-                </div>       
+					<div class="sorting-left">
+						<select id="items" onchange="javascript:handleSelelect(this)" class="frm-field required sect">
+							<option value="items9">Items por página 9</option>
+							<option value="items18">Items por página 18</option> 
+							<option value="items32">Items por página 32</option>					
+							<option value="itemsTodos">Todo</option>								
+						</select>
+					</div>
+            		<div class="clearfix"> </div>
+        		</div>
+    		  </div> 
 				
 @php 
 $colmd = 4;
@@ -79,5 +79,12 @@ $colmd = 4;
 @stop
 
 @section('js')
-    <script> </script>
+    <script>
+
+		function handleSelect(elm)
+		{
+			
+		}
+
+	</script>
 @stop
--- resources/views/Site/components/product.blade.php
+++ resources/views/Site/components/product.blade.php
@@ -4,11 +4,11 @@
 				<div class="hover14 column">
 					<div class="agile_top_brand_left_grid">
 						<div class="agile_top_brand_left_grid_pos">
-							@if ($product->offer == 1)<img style="width:27px;" src="/site/images/rebaja.png" alt="Oferta" class="img-responsive">@endif
-							@if ($product->organic == 1)<img style="width:30px;" alt="Organicos" src="/site/images/organicos.png" class="img-responsive">@endif
-							@if ($product->agroecological == 1)<img style="width:30px;" src="/site/images/agroecologicos.png" alt="Agroecologicos" class="img-responsive">@endif
-							@if ($product->celiacs == 1)<img style="width:30px;" src="/site/images/celiacs.png" alt="Sin tacc" class="img-responsive">@endif
-							@if ($product->vegan == 1)<img style="width:30px;" src="/site/images/veganos.png" alt="Veganos" class="img-responsive">@endif
+							@if ($product->offer == 1)<img style="width:27px;" src="/site/images/rebaja.png" title="Oferta" class="img-responsive">@endif
+							@if ($product->organic == 1)<img style="width:30px;" src="/site/images/organicos.png" title="Organicos" class="img-responsive">@endif
+							@if ($product->agroecological == 1)<img style="width:30px;" src="/site/images/agroecologicos.png" title="Agroecologicos" class="img-responsive">@endif
+							@if ($product->celiacs == 1)<img style="width:30px;" src="/site/images/celiacs.png" title="Sin tacc" class="img-responsive">@endif
+							@if ($product->vegan == 1)<img style="width:30px;" src="/site/images/veganos.png" title="Veganos" class="img-responsive">@endif
 						</div>
 						<div class="agile_top_brand_left_grid1">
 						<figure>
