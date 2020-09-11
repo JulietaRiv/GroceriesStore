50dc0b4e33bbc35e7e931f3be605e7936ab745c0
JulietaRiv <positivejvr@gmail.com>
2020-09-11 18:36:38 -0300

    banner ok

--- resources/views/Site/banner.blade.php
+++ resources/views/Site/banner.blade.php
@@ -3,17 +3,17 @@
 	<div class="container">
 		<div class="col-md-6 ban-bottom3">
 			<div class="ban-top">
-				<img src="site/images/essential-oil.jpg" class="img-responsive" alt="">	
+				<img src="site/images/separador3.jpg" class="img-responsive" alt="">	
 			</div>
 			<div class="ban-img">
 				<div class=" ban-bottom1">
 					<div class="ban-top">
-						<img src="site/images/juice.jpg" class="img-responsive" alt="">							
+						<img src="site/images/separador1.jpg" class="img-responsive" alt="">							
 					</div>
 				</div>
 				<div class="ban-bottom2">
 					<div class="ban-top">
-						<img src="site/images/polen.jpg" class="img-responsive" alt="">										
+						<img src="site/images/separador2.jpg" class="img-responsive" alt="">										
 					</div>
 				</div>
 				<div class="clearfix"></div>
@@ -21,7 +21,7 @@
 		</div>
 		<div class="col-md-6 ban-bottom">
 			<div class="ban-top">
-				<img src="site/images/honey.jpg" class="img-responsive" alt="">														
+				<img src="site/images/separador4.jpg" class="img-responsive" alt="">														
 			</div>
 		</div>					
 		<div class="clearfix"></div>
