97454bbfccf8ba993782d9efce2f088e937cdf1e
JulietaRiv <positivejvr@gmail.com>
2020-09-28 14:06:32 -0300

    logo nuevo

--- resources/views/Site/layout.blade.php
+++ resources/views/Site/layout.blade.php
@@ -66,7 +66,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				</ul>
 			</div>
 			<div class="w3ls_logo_products_left">
-				<a href="/"><img style="width:200px;" src="/site/images/logo-roble.jpg" alt="El Roble"/></a>
+				<a href="/"><img style="width:200px;" src="/site/images/logo2-roble.png" alt="El Roble"/></a>
 			</div>
 			<div class="w3l_search">
 				<form id="searchForm" action="{{Route('siteProducts', ['search', 'q'])}}" method="get">
